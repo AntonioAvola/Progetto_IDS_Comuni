85c02275e36f0bb5fbcb5be11cd639a7df1598e3
Antonio Avola <antonio.avola@studenti.unicam.it>
2024-11-23 18:58:32 +0100

    [MODIFIED] contesto di sicurezza

--- Progetto_Java/src/main/java/com/unicam/Security/JwtRequestFilter.java
+++ Progetto_Java/src/main/java/com/unicam/Security/JwtRequestFilter.java
@@ -76,17 +76,6 @@ public class JwtRequestFilter extends OncePerRequestFilter {
             }
         }
 
-//        String header = request.getHeader("Authorization");
-//        if (header != null && header.startsWith("Bearer ")) {
-//            String token = header.substring(7);
-//            if (jwtUtils.validateToken(token)) {
-//                String username = jwtUtils.getRoleFromToken(token);
-//                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-//                SecurityContextHolder.getContext().setAuthentication(
-//                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities())
-//                );
-//            }
-//        }
         filterChain.doFilter(request, response);
     }
 
--- Progetto_Java/src/main/java/com/unicam/Security/SecurityConfig.java
+++ Progetto_Java/src/main/java/com/unicam/Security/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -28,7 +29,6 @@ public class SecurityConfig  {
     public SecurityConfig(JwtRequestFilter jwtRequestFilter) {
         this.jwtRequestFilter = jwtRequestFilter;
     }
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
@@ -36,12 +36,11 @@ public class SecurityConfig  {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(WHITE_LIST_URL).permitAll()
-                        .anyRequest().authenticated()
+                        .anyRequest().permitAll()
                 )
                 .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
