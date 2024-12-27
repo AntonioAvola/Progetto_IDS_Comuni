38f40e0dedb38faca97273ec56fef8b4f277667e
SofiaScarpeccio <sofia.scarpeccio@studenti.unicam.it>
2024-12-27 22:21:33 +0100

    [ADD] aggiunta test Autenticazione Fallita e Creazione Contest Eventi

--- Progetto_Java/src/test/java/com/unicam/test/AnimatorTest/AnimatorTest.java
+++ Progetto_Java/src/test/java/com/unicam/test/AnimatorTest/AnimatorTest.java
@@ -1,12 +1,20 @@
 package com.unicam.test.AnimatorTest;
 
 import com.unicam.Security.DataInizializer;
+import com.unicam.test.SecurityContext.WithMockUserDetails;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -22,4 +30,42 @@ public class AnimatorTest {
     public void setUp() throws Exception{
         dataInitializer.run();
     }
+
+    @Test
+    @WithMockUserDetails(username = "animator1", idUser = 4,  municipality = "MILANO", roles = "ANIMATOR", visitedMunicipality = "MILANO")
+    void testAddEventSuccessful() throws Exception {
+        mockMvc.perform(post("/api/animator/add/event")
+                        .contentType(APPLICATION_JSON)
+                        .content("""
+				 {
+					"title": "stelle cadenti",
+					"description": "osservazione stelle cadenti",
+					"reference": "torre velasca",
+					"start": "2025-02-20T20:00:00",
+					"end": "2025-02-21T03:00:00"
+				}
+				""")) // Corpo della richiesta
+                .andExpect(status().isOk()) //risposta HTTP 200 OK
+                .andExpect(content().string("Proposta di evento inviata con successo"));
+    }
+
+    @Test
+    @WithMockUserDetails(username = "animator1", idUser = 4,  municipality = "MILANO", roles = "ANIMATOR", visitedMunicipality = "MILANO")
+    void testAddContestSuccessful() throws Exception {
+        mockMvc.perform(post("/api/animator/add/contest")
+                        .contentType(APPLICATION_JSON)
+                        .content("""
+				 {
+					"title": "fotografia",
+					"description": "fotografia più bella",
+					"reward": "€150",
+					"start": "2025-02-25T16:00:00",
+					"end": "2025-02-28T18:00:00"
+				}
+				""")) // Corpo della richiesta
+                .andExpect(status().isOk()) //risposta HTTP 200 OK
+                .andExpect(content().string("Proposta di contest inviata con successo"));
+    }
+
+
 }
--- Progetto_Java/src/test/java/com/unicam/test/AuthTest/AuthTest.java
+++ Progetto_Java/src/test/java/com/unicam/test/AuthTest/AuthTest.java
@@ -54,4 +54,174 @@ public class AuthTest {
                         .param("role", "CONTRIBUTOR"))
                 .andExpect(status().isOk());
     }
+
+    @Test
+    void testLoginFailed() throws Exception {
+        mockMvc.perform(post("/api/auth/login")
+                        .param("username", "adin")
+                        .param("password", "Admin1!"))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message").value("Non esiste alcun utente con l'username passato"));
+
+        mockMvc.perform(post("/api/auth/login")
+                        .param("username", "admin")
+                        .param("password", "Ad1!"))
+                .andExpect(status().isUnauthorized())
+                .andExpect(jsonPath("$.message").value("Password errata"));
+
+        mockMvc.perform(post("/api/auth/login")
+                        .param("username", "admin")
+                        .param("password", "Admin!"))
+                .andExpect(status().isUnauthorized())
+                .andExpect(jsonPath("$.message").value("Password errata"));
+    }
+
+    @Test
+    void testSingInFailed() throws Exception {
+        //username già in uso
+        mockMvc.perform(post("/api/auth/singIn")
+                        .contentType(APPLICATION_JSON)
+                        .content("""
+					{
+						"username": "admin",
+						"name": "elisa",
+						"email": "eli@gmail.com",
+						"password": "Au12!",
+						"municipality": "Roma"
+					}
+					""")
+                        .param("role", "CONTRIBUTOR"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("L'username è già in uso"));
+
+        //email già in uso
+        mockMvc.perform(post("/api/auth/singIn")
+                        .contentType(APPLICATION_JSON)
+                        .content("""
+					{
+						"username": "eli",
+						"name": "elisa",
+						"email": "admin@admin.com",
+						"password": "Au12!",
+						"municipality": "Roma"
+					}
+					""")
+                        .param("role", "CONTRIBUTOR"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("L'email è già in uso"));
+
+        //campo username vuoto o solamente spazi
+        mockMvc.perform(post("/api/auth/singIn")
+                        .contentType(APPLICATION_JSON)
+                        .content("""
+					{
+						"username": "",
+						"name": "elisa",
+						"email": "eli@gmail.com",
+						"password": "Au12!",
+						"municipality": "Roma"
+					}
+					""")
+                        .param("role", "CONTRIBUTOR"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("L'username non è stato inserito"));
+
+        //campo name vuoto o solamente spazi
+        mockMvc.perform(post("/api/auth/singIn")
+                        .contentType(APPLICATION_JSON)
+                        .content("""
+					{
+						"username": "eli",
+						"name": "",
+						"email": "eli@gmail.com",
+						"password": "Au12!",
+						"municipality": "Roma"
+					}
+					""")
+                        .param("role", "CONTRIBUTOR"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("Il nome non è stato inserito"));
+
+        //campo email vuoto o solamente spazi
+        mockMvc.perform(post("/api/auth/singIn")
+                        .contentType(APPLICATION_JSON)
+                        .content("""
+					{
+						"username": "eli",
+						"name": "elisa",
+						"email": "",
+						"password": "Au12!",
+						"municipality": "Roma"
+					}
+					""")
+                        .param("role", "CONTRIBUTOR"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("L'email non è stata inserita"));
+
+        //email formato non corretto
+        mockMvc.perform(post("/api/auth/singIn")
+                        .contentType(APPLICATION_JSON)
+                        .content("""
+					{
+						"username": "eli",
+						"name": "elisa",
+						"email": "eligmail.com",
+						"password": "Au12!",
+						"municipality": "Roma"
+					}
+					""")
+                        .param("role", "CONTRIBUTOR"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("L'email non è stata inserita correttamente. " +
+                        "Si prega di inserire una email valida"));
+
+        //campo password vuoto o solamente spazi
+        mockMvc.perform(post("/api/auth/singIn")
+                        .contentType(APPLICATION_JSON)
+                        .content("""
+					{
+						"username": "eli",
+						"name": "elisa",
+						"email": "eli@gmail.com",
+						"password": "     ",
+						"municipality": "Roma"
+					}
+					""")
+                        .param("role", "CONTRIBUTOR"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("La password non è stata inserita"));
+
+        //password che non rispetta tutti i requisiti
+        mockMvc.perform(post("/api/auth/singIn")
+                        .contentType(APPLICATION_JSON)
+                        .content("""
+					{
+						"username": "eli",
+						"name": "elisa",
+						"email": "eli@gmail.com",
+						"password": "Au1!",
+						"municipality": "Roma"
+					}
+					""")
+                        .param("role", "CONTRIBUTOR"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("La password non rispetta i requisiti richiesti: lunghezza 5, almeno una maiuscola, " +
+                        "almeno una minuscola, almeno un numero, almeno un simbolo speciale, niente spazi vuoti"));
+
+        //campo municipality vuoto o solamente spazi
+        mockMvc.perform(post("/api/auth/singIn")
+                        .contentType(APPLICATION_JSON)
+                        .content("""
+					{
+						"username": "eli",
+						"name": "elisa",
+						"email": "eli@gmail.com",
+						"password": "Au12!",
+						"municipality": ""
+					}
+					""")
+                        .param("role", "CONTRIBUTOR"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("Il Comune di residenza non è stato inserito"));
+    }
 }
