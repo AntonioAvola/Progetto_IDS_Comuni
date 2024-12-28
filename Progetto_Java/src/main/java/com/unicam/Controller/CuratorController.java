e214c19654c3e8b593aa76ed73122df2481a98f7
SofiaScarpeccio <sofia.scarpeccio@studenti.unicam.it>
2024-12-28 18:06:14 +0100

    [ADD] aggiunta API validazione contenuti segnalati

--- Progetto_Java/src/main/java/com/unicam/Controller/CuratorController.java
+++ Progetto_Java/src/main/java/com/unicam/Controller/CuratorController.java
@@ -60,10 +60,10 @@ public class CuratorController {
         return ResponseEntity.ok(pending);
     }
 
-    @PutMapping("/approve/or/reject/content/or/report")
-    @Operation(summary = "Approva o rifiuta un contenuto o segnalazione",
-            description = "Approva o rifiuta un contenuto in attesa o segnalazione. Usa uno degli ID disponibili da /getContentPending.")
-    public ResponseEntity<String> ValidatePendingOrReported(
+    @PutMapping("/approve/or/reject/content")
+    @Operation(summary = "Approva o rifiuta un contenuto",
+            description = "Approva o rifiuta un contenuto in attesa. Usa uno degli ID disponibili da /getContentPending.")
+    public ResponseEntity<String> ValidatePending(
             @Parameter(description = "Tipo di contenuto",
                     schema = @Schema(type = "String", allowableValues = {"INTEREST POINT", "ITINERARY"}))
             @RequestParam(defaultValue = "INTEREST POINT") String type,
@@ -122,5 +122,47 @@ public class CuratorController {
         Response.getContents().put("InterestPoint", Point);
         Response.getContents().put("Itinerary", Itinerary);
         return ResponseEntity.ok(Response);
+    }
+
+    @PutMapping("/approve/or/reject/report")
+    @Operation(summary = "Approva o rifiuta un contenuto segnalato",
+            description = "Approva o rifiuta un contenuto segnalato. Usa uno degli ID disponibili da /ViewAllReportedContent.")
+    public ResponseEntity<String> ValidateReported(
+            @Parameter(description = "Tipo di contenuto",
+                    schema = @Schema(type = "String", allowableValues = {"INTEREST POINT", "ITINERARY"}))
+            @RequestParam(defaultValue = "INTEREST POINT") String type,
+            @RequestParam long idContent,
+            @Parameter(description = "Operazione da eseguire",
+                    schema = @Schema(type = "ContentStatus", allowableValues = {"APPROVED", "REJECTED"}))
+            @RequestParam(defaultValue = "APPROVED") ContentStatus status){
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        UserCustomDetails userDetails = (UserCustomDetails) authentication.getPrincipal();
+
+        String username = userDetails.getUsername();
+        String id = userDetails.getId();
+        long idUser = Long.parseLong(id);
+        String role = userDetails.getRole();
+        String municipality = userDetails.getMunicipality();
+        String visitedMunicipality = userDetails.getVisitedMunicipality();
+
+        //TODO controllo comune:
+        // se comune visitato è lo stesso del proprio comune allora proseguire;
+        // altrimenti eccezione
+
+        //TODO controllo ruolo
+
+        if(type.equals("INTEREST POINT")){
+            if(!this.interestPointService.checkMunicipality(idContent, municipality))
+                throw new IllegalArgumentException("Punto di interesse non appartenente al comune di " + municipality);
+            this.interestPointService.approveOrRejectPoint(idContent, status);
+        }
+        else {
+            if(!this.itineraryService.checkMunicipality(idContent, municipality))
+                throw new IllegalArgumentException("Itinerario non appartenente al comune di " + municipality);
+            this.itineraryService.approveOrRejectItinerary(idContent, status);
+        }
+        return ResponseEntity.ok("Operazione eseguita con successo");
     }
 }
